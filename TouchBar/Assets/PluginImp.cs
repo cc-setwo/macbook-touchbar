83e5014676d4e9491030e774a02f4bdd0f22a286
vladislav.garkusha <vladislav.garkusha@muka.com>
2019-01-29 15:22:28 +0200

    Unity project added.

---
+++ TouchBar/Assets/PluginImp.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+[ExecuteInEditMode]
+public class PluginImp : MonoBehaviour {
+
+	[DllImport ("tb-bundle")]
+	private static extern int InitTB ();
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.LogError(InitTB());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
